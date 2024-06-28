2145bebcebd0c6bf69d1d79e97acfc18520dd106
Yannic Eising <yeising@students.uni-mainz.de>
2024-06-28 14:54:16 +0200

    fix: unconditional uncompress

--- workflow/scripts/read_filter.py
+++ workflow/scripts/read_filter.py
@@ -9,12 +9,19 @@ import gzip
 sys.stderr = sys.stdout = open(snakemake.log[0], "wt")
 
 if snakemake.config["min_length"] == 0:
-    snakemake.output = snakemake.input
+    with gzip.open(snakemake.input[0], "rt") as sample, open(
+        snakemake.output[0]
+    ) as out:
+        for line in sample:
+            out.write(line)
 else:
-    with gzip.open(snakemake.input[0], "rt") as sample : 
+    with gzip.open(snakemake.input[0], "rt") as sample:
         input_iterator = SeqIO.parse(sample, "fastq")
 
-        filter_iterator = (read for read in input_iterator if len(read.seq) >= snakemake.config["min_length"])
-
+        filter_iterator = (
+            read
+            for read in input_iterator
+            if len(read.seq) >= snakemake.config["min_length"]
+        )
 
         SeqIO.write(filter_iterator, snakemake.output[0], "fastq")
