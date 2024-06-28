2dd163720ae066008eb3c7eb93cbaf77997b2d36
Yannic Eising <yeising@students.uni-mainz.de>
2024-06-28 15:16:31 +0200

    fix: added .fq functionality

--- workflow/scripts/read_filter.py
+++ workflow/scripts/read_filter.py
@@ -4,18 +4,27 @@
 import sys
 from Bio import SeqIO
 import gzip
+import shutil
 
 # start logging
 sys.stderr = sys.stdout = open(snakemake.log[0], "wt")
 
+
+def is_zipped(fname):
+    return fname.endswith(".gz")
+
+
 if snakemake.config["min_length"] == 0:
-    with gzip.open(snakemake.input[0], "rt") as sample, open(
-        snakemake.output[0]
-    ) as out:
-        for line in sample:
-            out.write(line)
+    if is_zipped(snakemake.input[0]):
+        with gzip.open(snakemake.input[0], "wb") as f_in, open(
+            snakemake.output[0], "wb"
+        ) as f_out:
+            shutil.copyfileobj(f_in, f_out)
+    else:
+        shutil.copy2(snakemake.input[0], snakemake.output[0])
 else:
-    with gzip.open(snakemake.input[0], "rt") as sample:
+    open_fh = gzip.open if is_zipped(snakemake.input[0]) else open
+    with open_fh(snakemake.input[0], "rt") as sample:
         input_iterator = SeqIO.parse(sample, "fastq")
 
         filter_iterator = (
