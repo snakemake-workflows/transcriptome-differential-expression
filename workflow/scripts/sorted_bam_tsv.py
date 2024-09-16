6e0e594438bad4db2ddf8a36d252af9879513555
Yannic Eising <yeising@students.uni-mainz.de>
2024-09-16 14:53:09 +0200

    fix: made file paths for TSV absolute

--- workflow/scripts/sorted_bam_tsv.py
+++ workflow/scripts/sorted_bam_tsv.py
@@ -1,11 +1,14 @@
 import pandas as pd
 import sys
+import os
 
 sys.stderr = sys.stdout = open(snakemake.log[0], "w")
 
-sorted_bam_files = snakemake.input.sorted_bams
-sample_names = snakemake.params.samples
+sorted_bams = snakemake.input.sorted_bams
+samples = snakemake.params.samples
 
-df = pd.DataFrame({"sorted_bam_path": sorted_bam_files, "sample_name": sample_names})
+abs_path=[os.path.abspath(bam) for bam in sorted_bams]
+
+df = pd.DataFrame({"sorted_bam_path": abs_path, "sample_name": samples})
 
 df.to_csv(snakemake.output[0], sep="\t", index=False, header=False)
