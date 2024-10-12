f0dc696affa1efb04679fd322a29a1b4585c0e5e
Yannic Eising <yeising@students.uni-mainz.de>
2024-10-12 14:49:45 +0200

    feat: script to construct FLAIR input TSV

--- workflow/scripts/reads_manifest.py
+++ workflow/scripts/reads_manifest.py
@@ -1,4 +1,3 @@
-import pandas as pd
 import sys
 import os
 from pathlib import Path
@@ -17,6 +16,10 @@ def get_sample_path(sample_name, inputdir, exts):
             return str(path)+ext
     return "File not found"
 
+#remove underscores from the name field because flair does not accept them
+samples_df["sample_clean"] = samples_df["sample"].str.replace("_", "", regex=False)
+
+#get the absolute  filepath for each sample
 samples_df["sample_path"]=samples_df["sample"].apply(lambda x: get_sample_path(x, snakemake.params.inputdir, exts))
 
-samples_df.to_csv(snakemake.output[0], sep="\t", index=False, header=False)
+samples_df[["sample_clean","condition","batch","sample_path"]].to_csv(snakemake.output[0], sep="\t", index=False, header=False)
