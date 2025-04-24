92652d174517c5dd5b37cec31a0335532428b67e
Yannic Eising <yeising@students.uni-mainz.de>
2025-04-24 12:04:43 +0200

    fix: resolve KeyError and correctly merge splice counts by referencce

--- workflow/scripts/merge_splice_counts.py
+++ workflow/scripts/merge_splice_counts.py
@@ -6,25 +6,25 @@ from functools import reduce
 
 sys.stderr = sys.stdout = open(snakemake.log[0], "w")
 
-dfs = {x: pd.read_csv(x, sep="\t", header=None, names=["ids", "Count"]) for x in snakemake.input.sample_counts}
+dfs = {
+    x: pd.read_csv(x, sep="\t", header=None, names=["read_id", "Count"])
+    for x in snakemake.input
+}
 
 ndfs = []
 for x, df in dfs.items():
-    df.Count = np.array(df.Count, dtype=int)
-    df = df[df.Count > 0]
-    df = df.sort_values(by=["Count"], ascending=False)
-
-    name = path.dirname(x).rsplit("/", 1)[-1]
-    if name.endswith("_counts"):
-        name = name.replace("_counts", "")
-
+    df["id"] = df["read_id"].apply(lambda s: s.split("_")[-1])
+    df["Count"] = df["Count"].astype(float)
+    df = df.groupby("id", as_index=False).sum()
+    name = path.basename(x).split("_counts.tsv")[0]
     df = df.rename(columns={"Count": name})
-    df = df[["ids", name]]
-    ndfs.append(df)
+    ndfs.append(df[["id", name]])
 
 df_merged = reduce(
-    lambda left, right: pd.merge(left, right, on="Reference", how="outer"), ndfs
+    lambda left, right: pd.merge(left, right, on="id", how="outer"), ndfs
 )
+df_merged = df_merged.fillna(0)
+for col in df_merged.columns[1:]:
+    df_merged[col] = df_merged[col].astype(float)
 
-df_merged = df_merged.fillna(0).astype({col: int for col in df_merged.columns if col != "Reference"})
-df_merged.to_csv(snakemake.output.samples_counts, sep="\t", index=False)
+df_merged.to_csv(snakemake.output.counts_matrix, sep="\t", index=False)
